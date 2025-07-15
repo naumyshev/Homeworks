882818badc77153506f2b139275975d5151ffd79
Aleksei Naumyshev <naumyshev@yandex.ru>
2025-07-15 13:22:03 +0300

    homework 2

--- Homeworks/main.swift
+++ Homeworks/main.swift
@@ -7,90 +7,13 @@
 
 import Foundation
 
-// Конкатинация:
-
-// Задание 1:
-
-let firstName = "Aleksei"
-let lastName = "Naumyshev"
-let fullName = firstName + " " + lastName
-print(fullName)
-
-// Задание 2:
-
-let age = 46
-var message = "Меня зовут " + firstName + ", и мне " + String(age) + " лет."
-print(message)
-
-// Задание 3:
-
-let numder1 = 5
-let number2 = 7
-
-print("Сумма чисел " + String(numder1) + " и " + String(number2) + " равна " + String(numder1 + number2) + ".")
-
-// Интерполяция строк
-
-// Задание 1:
-
-message = "Меня зовут \(firstName), и мне \(age) лет."
-print(message)
-
-// Задание 2:
-
-let weight = 78.0
-let height = 1.73
-print("Ваш ИМТ равен \(weight / (height * height)).")
-
-// Задание 3:
-
-let productName = "Coffee"
-let price = 15
-let quantity = 2
-print("Вы добавили в корзину \(quantity) шт. товара '\(productName)', стоимость которого равна \(price) на сумму \(price * quantity) руб.")
-
-// Основные типы данных:
-
-// Задание 1:
-
-let numderInt16: Int16 = -32768
-let numberUnInt8: UInt8 = 200
-let Pi: Double = 3.1415926535
-let isDone: Bool = false
-let greeting: String = "Hello, world!"
-
-// Задание 2:
-
-let number: Int = 42
-print(number * number)
-
-// Задание 3:
-
-var number3 = 5
-var isEven = number3 % 2 == 0
-
-// Переменные и константы:
-
-// Задание 1:
-
-var temperature = 12
-temperature = 18
-
-// Задание 2:
-
-let  birthYear = 1979
-//birthYear = 1980
-
-// Задание 3:
-
-var  count = 10
-//count = "Hi!"
+// ДЗ 2
 
 // Логические значения и условные операторы
 
 // Задание 1:
 
-var testNumber = 5
+var testNumber = 0
 if testNumber > 0 {
     print("\(testNumber) is positive")
 } else if testNumber < 0 {
@@ -107,3 +30,149 @@ if userAge >= 18 {
 } else {
     print("Доступ запрещен.")
 }
+
+// Задание 3:
+
+var number = 6
+if number % 2 == 0 {
+    print("Число \(number) чётное")
+} else {
+    print("Число \(number) нечётное")
+}
+
+// Задание 4:
+
+let login = "admin"
+let pass = "12345"
+
+if login == "admin" && pass == "1234" {
+        print( "Доступ разрешен.")
+    } else {
+        print("Доступ запрещен.")
+    }
+
+// Опционалы
+
+// Задание 1:
+
+var optionalName: String? = "Алексей"
+//optionalName = nil
+print("Здравствуйте, \(optionalName!)!")
+
+// Задание 2:
+
+var optionalNumber: Int? = 10 // nil
+number = optionalNumber ?? 0
+print(number)
+
+// Задание 3:
+
+let age = 20
+let minAge = 18
+let testPassed = true
+
+let canObtainLicense = age >= minAge && testPassed
+
+print("Может получить права: \(canObtainLicense)")
+
+
+//// Конкатинация:
+//
+//// Задание 1:
+//
+//let firstName = "Aleksei"
+//let lastName = "Naumyshev"
+//let fullName = firstName + " " + lastName
+//print(fullName)
+//
+//// Задание 2:
+//
+//let age = 46
+//var message = "Меня зовут " + firstName + ", и мне " + String(age) + " лет."
+//print(message)
+//
+//// Задание 3:
+//
+//let numder1 = 5
+//let number2 = 7
+//
+//print("Сумма чисел " + String(numder1) + " и " + String(number2) + " равна " + String(numder1 + number2) + ".")
+//
+//// Интерполяция строк
+//
+//// Задание 1:
+//
+//message = "Меня зовут \(firstName), и мне \(age) лет."
+//print(message)
+//
+//// Задание 2:
+//
+//let weight = 78.0
+//let height = 1.73
+//print("Ваш ИМТ равен \(weight / (height * height)).")
+//
+//// Задание 3:
+//
+//let productName = "Coffee"
+//let price = 15
+//let quantity = 2
+//print("Вы добавили в корзину \(quantity) шт. товара '\(productName)', стоимость которого равна \(price) на сумму \(price * quantity) руб.")
+//
+//// Основные типы данных:
+//
+//// Задание 1:
+//
+//let numderInt16: Int16 = -32768
+//let numberUnInt8: UInt8 = 200
+//let Pi: Double = 3.1415926535
+//let isDone: Bool = false
+//let greeting: String = "Hello, world!"
+//
+//// Задание 2:
+//
+//let number: Int = 42
+//print(number * number)
+//
+//// Задание 3:
+//
+//var number3 = 5
+//var isEven = number3 % 2 == 0
+//
+//// Переменные и константы:
+//
+//// Задание 1:
+//
+//var temperature = 12
+//temperature = 18
+//
+//// Задание 2:
+//
+//let  birthYear = 1979
+////birthYear = 1980
+//
+//// Задание 3:
+//
+//var  count = 10
+////count = "Hi!"
+//
+//// Логические значения и условные операторы
+//
+//// Задание 1:
+//
+//var testNumber = 5
+//if testNumber > 0 {
+//    print("\(testNumber) is positive")
+//} else if testNumber < 0 {
+//    print("\(testNumber) is negative")
+//} else {
+//    print("\(testNumber) is zero")
+//}
+//
+//// Задание 2:
+//
+//let userAge = 18
+//if userAge >= 18 {
+//    print( "Доступ разрешен.")
+//} else {
+//    print("Доступ запрещен.")
+//}
